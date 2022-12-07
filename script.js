8574ccf09005a97179320b13e07c7fd258622b90
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2022-12-07 21:18:28 +0100

    cleaning

---
+++ script.js
@@ -0,0 +1,16 @@
+// let btnLeft = document.querySelector("#btn-left");
+// let btnRight = document.querySelector("#btn-right");
+// let projects = document.querySelector("#projects");
+
+// // btnLeft.addEventListener("mouseover", () => {projects.style.display = "block";});
+// // btnLeft.addEventListener("mouseout", () => {projects.style.display = "none";});
+
+// btnLeft.addEventListener("click", () => {
+//     if(getComputedStyle(projects).display != "none"){
+//         projects.style.display = "none";
+//     } else {
+//         projects.style.display = "block";
+//     }
+// })
+
+// A vérifiier pour masquer/afficher la section
