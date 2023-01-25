f779a22e3248b763089b891fbf3897f4342be061
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2023-01-25 23:27:16 +0100

    update

--- test.js
+++ test.js
@@ -1,11 +1,35 @@
-const burger = document.querySelector('.menu-burger');
-const nav = document.querySelector('.menu-nav');
-const close = document.querySelector('.close');
-
-burger.addEventListener('click', () => {
-  nav.classList.toggle('active');
-});
-
-// close.addEventListener('click', () => {
-//     nav.classList.remove('active');
+// const items = document.querySelectorAll(".item");
+// const expand = (item, i) => {
+//   items.forEach((it, ind) => {
+//     if (i === ind) return;
+//     it.clicked = false;
+//   });
+//   gsap.to(items, {
+//     width: item.clicked ? "15vw" : "8vw",
+//     duration: 2,
+//     ease: "elastic(1, .6)"
+//   });
+//   item.clicked = !item.clicked;
+//   gsap.to(item, {
+//     width: item.clicked ? "42vw" : "15vw",
+//     duration: 2.5,
+//     ease: "elastic(1, .3)"
+//   });
+// };
+// items.forEach((item, i) => {
+//   item.clicked = false;
+//   item.addEventListener("click", () => expand(item, i));
 // });
+
+const items = document.querySelectorAll(".item");
+const expand = (item, i) => {
+  items.forEach((it, ind) => {
+    if (i === ind) return;
+    it.classList.remove("expanded");
+  });
+  item.classList.toggle("expanded");
+};
+
+items.forEach((item, i) => {
+  item.addEventListener("click", () => expand(item, i));
+});
