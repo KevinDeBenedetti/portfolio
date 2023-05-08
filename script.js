c7fffdce27a112df76036d155b7d091b59feb2f5
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2023-05-09 01:10:40 +0200

    updates

--- script.js
+++ script.js
@@ -1,15 +1,67 @@
-import caroussel from "./ressources/caroussel.js";
-import burgerMenu from "./ressources/burger-menu.js";
-import modals from "./ressources/modals.js";
-import stars from "./ressources/stars.js";
-import map from "./ressources/map.js";
-import terminal from "./ressources/terminal.js";
+import { addClassOnHover, removeClassOnHover } from "./ressources/hover.js";
 
-caroussel();
-burgerMenu();
-modals();
-stars();
-map();
-terminal();
+document.addEventListener("DOMContentLoaded", () => {
+    const linkElements = document.querySelectorAll(".article-item__links a");
+    linkElements.forEach((link) => {
+        link.addEventListener("mouseover", () => addClassOnHover(link));
+        link.addEventListener("mouseout", () => removeClassOnHover(link));
+    });
+});
 
-console.log(terminal());
+  document.addEventListener("DOMContentLoaded", function () {
+    const h3Elements = document.querySelectorAll("h3");
+    const h3DummyElements = document.querySelectorAll(".h3-dummy");
+  
+    function checkScroll() {
+      h3Elements.forEach((h3, index) => {
+        const rect = h3.getBoundingClientRect();
+        const isVisible = rect.top >= 0 && rect.bottom <= window.innerHeight;
+  
+        if (isVisible) {
+          h3.style.position = "static";
+          h3DummyElements[index].style.display = "none"; // Hide the dummy element
+  
+          // Reset other styles
+          // ...
+          h3.style.textTransform = "uppercase";
+          h3.style.color = "var(--text-white)";
+          h3.style.paddingTop = "24px"
+        } else {
+          h3.style.position = "sticky";
+          h3DummyElements[index].style.display = "block"; // Show the dummy element
+          h3DummyElements[index].style.height = h3.offsetHeight + "px"; // Set the height of the dummy element
+  
+          // Apply the desired styles
+          // ...
+          h3.style.top = "0";
+          h3.style.left = "0";
+          h3.style.width = "100%";
+          h3.style.backgroundColor = "black";
+          h3.style.zIndex = "1000";
+
+          h3.style.fontWeight = "bold";
+
+
+        }
+      });
+    }
+  
+    window.addEventListener("scroll", checkScroll);
+    checkScroll();
+  });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const navItems = document.querySelectorAll(".nav-item");
+
+    navItems.forEach((item) => {
+        item.addEventListener("mouseover", () => {
+            const bar = item.querySelector(".bar");
+            bar.style.width = "60px";
+        });
+
+        item.addEventListener("mouseout", () => {
+            const bar = item.querySelector(".bar");
+            bar.style.width = "20px";
+        });
+    });
+});
