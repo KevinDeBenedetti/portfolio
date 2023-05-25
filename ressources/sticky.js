aa11691786d8eb25e546b010ae51ec08b03ee1c8
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2023-05-25 23:00:55 +0200

    debug intersection

--- ressources/sticky.js
+++ ressources/sticky.js
@@ -11,21 +11,36 @@ export function initializeStickyTitle() {
     threshold: 0.5, // pour déclencher l'événement immédiatement
     rootMargin: "400px 0px" // pour déclencher l'événement 200px avant
     };
+    
+    let currentSection = null;
+
+    const callback = entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const title = entry.target.querySelector('h3').innerText;
+                stickyHeader.innerText = title;
+                stickyHeader.classList.add('visible');
+                currentSection = entry.target;
+            } else if (entry.target === currentSection) {
+                stickyHeader.classList.remove('visible');
+                currentSection = null;
+            }
+        });
+    };
 
     // Callback pour l'Intersection Observer
-    const callback = entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-        console.log(entry)
-        // Mettre à jour le sticky header avec le titre de la section actuellement visible
-        const title = entry.target.querySelector('h3').innerText;
-        stickyHeader.innerText = title;
-        stickyHeader.classList.add('visible');
-        } else {
-        stickyHeader.classList.remove("visible");
-        }
-    });
-    };
+    // const callback = entries => {
+    // entries.forEach(entry => {
+    //     if (entry.isIntersecting) {
+    //     // Mettre à jour le sticky header avec le titre de la section actuellement visible
+    //     const title = entry.target.querySelector('h3').innerText;
+    //     stickyHeader.innerText = title;
+    //     stickyHeader.classList.add('visible');
+    //     } else {
+    //     stickyHeader.classList.remove("visible");
+    //     }
+    // });
+    // };
 
     // Créer l'Intersection Observer
     const observer = new IntersectionObserver(callback, options);
